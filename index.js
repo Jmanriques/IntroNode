a3e8fca7423a5313fdf911c77ef548cb295cf965
jmanrique <jmanriquesuarez@gmail.com>
2022-10-24 20:47:12 -0500

    API temperatura

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("prueba");
+//cargar libreria
+const axios = require("axios");
+
+// creo variables para la llave de la Api y 
+// para la ciudad de donde quiero saber el clima
+
+let apiKey = "28ca4968891cb6bd7b6d13742cb4f624";
+let city = "Bogota, col";
+
+// se crea el objeto de llamada 
+let requestconfig = {
+    url:
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city + "&appid=" + apiKey,
+};
+
+// creación de objeto promesa
+let objPromesa = axios(requestconfig);
+
+objPromesa
+    .then(function(response){
+        console.log(
+            "la temperatura de Bogota es: " +
+            (response.data.main.temp -273.15).toFixed(2).toString()+ " grados celcius");
+    })
+    .catch(function(err){
+        console.log(err)
+    });
